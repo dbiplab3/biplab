d486761ac53a4eb3a535ed7ebf84a44e0298fb39
dbiplab3 <97376018+dbiplab3@users.noreply.github.com>
2022-02-26 17:54:58 +0000

    my 1st commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"login",component:LoginComponent},
+  {path:"dashboard",component:DashboardComponent} 
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
